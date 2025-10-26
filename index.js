331f7002ba6bc67b7969bdf5033ddb6d3b828e86
Amit Raj <amitkraj20@gmail.com>
2025-10-26 12:26:29 +0530

    Adding new file

--- index.js
+++ index.js
@@ -1,68 +1,4 @@
-// const apiKey="a55e3228bf40c319d6c1a109f1205579";
-// // const apiUrl ="https://api.openweathermap.org/data/2.5/weather?q=&units=metric&q=";
-// const apiUrl ="https://api.openweathermap.org/data/2.5/weather?q=";
-
-// let searchInput=document.querySelector(".search input");
-// let searchBtn=document.querySelector(".search button");
-
-// let weather=document.querySelector(".weather");
-// let errorMsg=document.querySelector(".error");
-// let city=document.querySelector(".city");
-// let temp=document.querySelector(".temp");
-// let humidity=document.querySelector(".humidity");
-// let wind=document.querySelector(".wind");
-// let weatherIcon=document.querySelector(".weather-icon");
-
-// async function checkWeather(cityName){
-//     const response =await fetch(apiUrl + cityName + "&units=metric" + "&appid=" + apiKey);
-//     // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`);
-    
-//     if(response.status == 404){
-//         errorMsg.style.display="block";
-//         weather.style.display="none";
-//     }
-//     else{
-//         let data =await response.json();
-//         console.log(data);
-//         city.innerHTML=data.name;
-//         temp.innerHTML=Math.round(data.main.temp) + "°C";
-//         humidity.innerHTML=data.main.humidity + "%";
-//         wind.innerHTML=data.wind.speed + "km/h";
-
-//         if(data.weather[0].main == "Clouds"){
-//             weatherIcon.src="cloudy.png";
-//         }
-//         else if(data.weather[0].main == "Clear"){
-//             weatherIcon.src="clear.png";
-//         }
-//         else if(data.weather[0].main == "Rain"){
-//             weatherIcon.src="rainy.png";
-//         }
-//         else if(data.weather[0].main == "Drizzle"){
-//             weatherIcon.src="drizzle.png";
-//         }
-//         else if(data.weather[0].main == "Mist"){
-//             weatherIcon.src="mist.png";
-//         }
-//         else if(data.weather[0].main == "Haze"){
-//             weatherIcon.src="mist.png";
-//         }
-//         else if(data.weather[0].main == "Snow"){
-//             weatherIcon.src="snow.png";
-//         }
-//         errorMsg.style.display="none";
-//         weather.style.display="block";
-//     }
-// }
-
-// searchBtn.addEventListener("click" ,()=>{
-//     checkWeather(searchInput.value);
-// })
-
-
-
-const apiKey = "a55e3228bf40c319d6c1a109f1205579";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+import { API_KEY , API_URL } from "./env.js";
 
 let searchInput = document.querySelector(".search input");
 let searchBtn = document.querySelector(".search button");
@@ -77,7 +13,7 @@ let weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(cityName) {
     try {
-        const response = await fetch(`${apiUrl}${cityName}&units=metric&appid=${apiKey}`);
+        const response = await fetch(`${API_URL}${cityName}&units=metric&appid=${API_KEY}`);
         
         if (!response.ok) {
             throw new Error('City not found');
